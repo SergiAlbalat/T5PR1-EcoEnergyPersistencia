1c3ff19b64859c5d4ce931bffcc420435da9d6d3
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-02-24 11:50:51 +0100

    Add project files.

---
+++ Models/SistemaEnergia.cs
@@ -0,0 +1,36 @@
+﻿namespace EcoEnergyRazorProject.Models
+{
+    public abstract class SistemaEnergia
+    {
+        protected string? Tipus { get; set; }
+        protected double Energia { get; set; }
+        protected DateTime Data { get; set; }
+        protected static int ContadorSimulacions = 0;
+
+        /// <summary>
+        /// Dona el constingut del atribut ContadorSimulacions
+        /// </summary>
+        /// <returns>Contingut de Contador Simulacions</returns>
+        public static int GetContador() => ContadorSimulacions;
+        protected const double ParametrePerDefecte = 20.0d;
+        protected const string ErrorForaRang = "L'argument es troba fora de rang";
+
+        /// <summary>
+        /// Dona el contingut del atribut Tipus
+        /// </summary>
+        /// <returns>Constingut de Tipus</returns>
+        public string? GetTipus() => Tipus;
+
+        /// <summary>
+        /// Dona el contingut del atribut Energia
+        /// </summary>
+        /// <returns>Contingut de Energia</returns>
+        public double GetEnergia() => Energia;
+
+        /// <summary>
+        /// Dona el contingut del atribut Data
+        /// </summary>
+        /// <returns>Contingut de Data</returns>
+        public DateTime GetData() => Data;
+    }
+}
