a0727a8f5bedcdd6365ace10c85f901c459034ea
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-03-19 10:41:07 +0100

    Llegeix correctament les dades Closes #6

--- Pages/IndicadorsEnergetics.cshtml.cs
+++ Pages/IndicadorsEnergetics.cshtml.cs
@@ -4,37 +4,21 @@ using EcoEnergyRazorProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Globalization;
+using EcoEnergyRazorProject.Data;
 
 namespace EcoEnergyRazorProject.Pages
 {
     public class IndicadorsEnergeticsModel : PageModel
     {
-        public bool FileExist { get; set; } = false;
         public bool HasRecords { get; set; } = false;
         public List<IndicadorEnergetic> IndicadorsEnergetics { get; set; } = new List<IndicadorEnergetic>();
         public void OnGet()
         {
-            string file = "wwwroot/Files/indicadors_energetics_cat.csv";
-            if (System.IO.File.Exists(file))
+            using var context = new AplicationDbContext();
+            IndicadorsEnergetics = context.IndicadorsEnergetics.ToList();
+            if (IndicadorsEnergetics.Count > 0)
             {
-                FileExist = true;
-                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-                {
-                    HasHeaderRecord = true,
-                };
-                using (var reader = new StreamReader(file))
-                using (var csv = new CsvReader(reader, config))
-                {
-                    var registres = csv.GetRecords<IndicadorEnergetic>();
-                    foreach (var indicador in registres)
-                    {
-                        IndicadorsEnergetics.Add(indicador);
-                    }
-                    if (IndicadorsEnergetics.Count > 0)
-                    {
-                        HasRecords = true;
-                    }
-                }
+                HasRecords = true;
             }
         }
     }
--- Pages/Simulacions.cshtml.cs
+++ Pages/Simulacions.cshtml.cs
@@ -7,38 +7,21 @@ using System;
 using System.IO;
 using CsvHelper;
 using CsvHelper.Configuration;
+using EcoEnergyRazorProject.Data;
 
 namespace EcoEnergyRazorProject.Pages
 {
     public class SimulacionsModel : PageModel
     {
         public List<Simulacio> LlistaSimulacions { get; set; } = new List<Simulacio>();
-        public  bool FileExist { get; set; } = false;
         public bool HasRecords { get; set; } = false;
         public void OnGet()
         {
-            string arxiu = "wwwroot/Files/simulacions_energia.csv";
-            
-            if (System.IO.File.Exists(arxiu))
+            using var context = new AplicationDbContext();
+            LlistaSimulacions = context.Simulacions.ToList();
+            if (LlistaSimulacions.Count() > 0)
             {
-                FileExist = true;
-                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-                {
-                    HasHeaderRecord = true,
-                };
-                using var reader = new StreamReader(arxiu);
-                using (var csv = new CsvReader(reader, config))
-                {
-                    var registres = csv.GetRecords<Simulacio>();
-                    foreach (var simulacio in registres)
-                    {
-                        LlistaSimulacions.Add(simulacio);
-                    }
-                    if(LlistaSimulacions.Count() > 0)
-                    {
-                        HasRecords = true;
-                    }
-                }
+                HasRecords = true;
             }
         }
     }
